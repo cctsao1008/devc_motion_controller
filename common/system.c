2923e8874a17a085804da4c51df046213c05e38b
cctsao1008 <chiacheng.tsao@gmail.com>
2016-06-30 03:19:57 +0800

    add "FreeRTOSv9.0.0".
    Signed-off-by: cctsao1008 <chiacheng.tsao@gmail.com>

--- common/system.c
+++ common/system.c
@@ -32,3 +32,40 @@ system_data* system_init(void)
 
 /* parameters */
 
+/* FreeRTOS */
+void vAssertCalled( unsigned long ulLine, const char * const pcFileName )
+{
+	
+}
+
+void vApplicationIdleHook( void )
+{
+	
+}
+
+void vApplicationMallocFailedHook( void )
+{
+	
+}
+
+void vApplicationTickHook( void )
+{
+	
+}
+
+void vConfigureTimerForRunTimeStats( void )
+{
+	/* This function is not used by the Blinky build configuration, but needs
+	to be defined as the Blinky and Full build configurations share a
+	FreeRTOSConfig.h header file. */
+}
+/*-----------------------------------------------------------*/
+
+unsigned long ulGetRunTimeCounterValue( void )
+{
+	/* This function is not used by the Blinky build configuration, but needs
+	to be defined as the Blinky and Full build configurations share a
+	FreeRTOSConfig.h header file. */
+	return 0UL;
+}
+
