5f009f64da8bc827d11e45c1deb5feb0302a81a5
tsao.ricardo <tsao.ricardo@iac.com.tw>
2016-07-01 01:13:37 +0800

    motor driver coding.
    Signed-off-by: tsao.ricardo <tsao.ricardo@iac.com.tw>

--- platform/omr10/motor_driver.c
+++ platform/omr10/motor_driver.c
@@ -20,123 +20,29 @@
 
 #define DEBUG true
 
+/* protocol */
+#define STC1 0xEB // start code 1
+#define STC2 0x90 // start code 2
+
 /* Add "\\\\.\\" for COM > 10 */
 static char* com_port = (char*)"\\\\.\\COM6";
 
-uint8_t bcc(char *data, uint16_t length)
+uint8_t bcc(char *data, uint16_t len)
 {
     int i = 0, bcc = 0xFF;
     
-    for(i = 0 ; i < length ; i++)
+    for(i = 0 ; i < len ; i++)
     {
         bcc ^= data[i];
     }
     
+    bcc = bcc & 0xFF;
+    
     #if DEBUG
-    MSG(sd->log, "[DEBUG] CRC-32 = 0X%x \n", bcc);
+    MSG(sd->log, "[DEBUG] CRC-32 = 0x%X \n", bcc);
     #endif
     
     return bcc;
-}
-
-/* variables used with the com port */
-BOOL            bPortReady;
-DCB             dcb;
-COMMTIMEOUTS    CommTimeouts;
-
-bool setup_port(HANDLE hComm, uint16_t baud, uint8_t data_bits, uint8_t stop_bits, bool parity, bool hardware_control)
-{
-    if(hComm == NULL)
-    {
-        return false;
-    }
-
-    /* struct termios options */
-    if(!SetupComm(hComm, 128, 128))
-    {
-        MSG(sd->log, "[ERROR] setup_port(SetupComm), failed! \n");
-        return false;
-    }
-
-    SecureZeroMemory(&dcb, sizeof(DCB));
-    dcb.DCBlength = sizeof(DCB);
-
-
-    if(!GetCommState(hComm, &dcb))
-    {
-        MSG(sd->log, "[ERROR] setup_port(GetCommState), failed! \n");
-        return false;
-    }
-
-    dcb.BaudRate = baud;
-    dcb.ByteSize = data_bits;
-    dcb.Parity = NOPARITY;
-    dcb.StopBits = data_bits;
-    dcb.fAbortOnError = TRUE;
-    dcb.fBinary = TRUE;
-
-    /*  set XON/XOFF */
-    dcb.fOutX = FALSE;
-    dcb.fInX = FALSE;
-    dcb.fOutxCtsFlow = FALSE;
-    dcb.fRtsControl = RTS_CONTROL_DISABLE;
-    dcb.fOutxDsrFlow = FALSE;
-    dcb.fDtrControl = DTR_CONTROL_DISABLE;
-
-    bPortReady = SetCommState(hComm, &dcb);
-
-    /* Communication timeouts are optional */
-    CommTimeouts.ReadIntervalTimeout = 0;
-    CommTimeouts.ReadTotalTimeoutConstant = 0;
-    CommTimeouts.ReadTotalTimeoutMultiplier = 500;
-    CommTimeouts.WriteTotalTimeoutConstant = 0;
-    CommTimeouts.WriteTotalTimeoutMultiplier = 500;
-
-    if(!SetCommTimeouts (hComm, &CommTimeouts))
-    {
-        MSG(sd->log, "[ERROR] setup_port(SetCommTimeouts), failed! \n");
-        return false;
-    }
-        
-    if(!GetCommState(hComm, &dcb))
-    {
-        MSG(sd->log, "[ERROR] setup_port(GetCommState), failed! \n");
-        return false;
-    }
-
-    MSG(sd->log, "[INFO] setup_port, passed. \n");
-    return true;
-}
-
-/**
-    Reading:
-    To tell windows that data is going to be send over the serial port, the handle, the data and the amount of data are needed.
-    The following transmission is a simple fileIO operation.
- */
-
-DWORD readFromSerialPort(HANDLE hComm, uint8_t * buffer, int buffersize)
-{
-    DWORD dwBytesRead = 0;
-    if(!ReadFile(hComm, buffer, buffersize, &dwBytesRead, NULL)){
-        //handle error
-    }
-    return dwBytesRead;
-}
-
-/**
-    Writing
-    The same information is needed when writing to the port.
- */
-
-DWORD writeToSerialPort(HANDLE hComm, uint8_t * data, int length)
-{
-
-    DWORD dwBytesRead = 0;
-    if(!WriteFile(hComm, data, length, &dwBytesRead, NULL)){
-        //handle error
-    }
-    return dwBytesRead;
-
 }
 
 HANDLE open_port(const char* com_port)
@@ -172,60 +78,105 @@ bool close_port(HANDLE hComm)
     return true;
 }
 
-#if 0
-BOOL WriteABuffer(HANDLE hComm, unsigned char * lpBuf, DWORD dwToWrite)
+/* variables used with the com port */
+BOOL            bPortReady;
+DCB             dcb;
+COMMTIMEOUTS    CommTimeouts;
+
+bool setup_port(HANDLE hComm, uint16_t baud, uint8_t data_bits, uint8_t parity, uint8_t stop_bits)
 {
-    OVERLAPPED osWrite = {0};
-    DWORD dwWritten;
-    DWORD dwRes;
-    BOOL fRes;
-
-    // Create this write operation's OVERLAPPED structure's hEvent.
-    osWrite.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-
-    if (osWrite.hEvent == NULL)
-        // error creating overlapped event handle
-        return FALSE;
-
-    // Issue write.
-    if (!WriteFile(hComm, lpBuf, dwToWrite, &dwWritten, &osWrite))
+    if(hComm == NULL)
     {
-        if (GetLastError() != ERROR_IO_PENDING) { 
-        // WriteFile failed, but isn't delayed. Report error and abort.
-        fRes = FALSE;
-        }
-        else
-            // Write is pending.
-            dwRes = WaitForSingleObject(osWrite.hEvent, INFINITE);
+        return false;
+    }
 
-        switch(dwRes)
-        {
-            // OVERLAPPED structure's event has been signaled. 
-            case WAIT_OBJECT_0 :
+    /* struct termios options */
+    if(!SetupComm(hComm, 128, 128))
+    {
+        MSG(sd->log, "[ERROR] setup_port(SetupComm), failed! \n");
+        return false;
+    }
 
-                if (!GetOverlappedResult(hComm, &osWrite, &dwWritten, FALSE))
-                    fRes = FALSE;
-                else
-                    // Write operation completed successfully.
-                    fRes = TRUE;
-                break;
-            
-            default :
-                // An error has occurred in WaitForSingleObject.
-                // This usually indicates a problem with the
-                // OVERLAPPED structure's event handle.
-                fRes = FALSE;
-                break;
-        }
-   }
-   else
-        // WriteFile completed immediately.
-        fRes = TRUE;
+    SecureZeroMemory(&dcb, sizeof(DCB));
+    dcb.DCBlength = sizeof(DCB);
 
-   CloseHandle(osWrite.hEvent);
-   return fRes;
+
+    if(!GetCommState(hComm, &dcb))
+    {
+        MSG(sd->log, "[ERROR] setup_port(GetCommState), failed! \n");
+        return false;
+    }
+
+    dcb.BaudRate = baud;
+    dcb.ByteSize = data_bits;
+    dcb.Parity = parity;
+    dcb.StopBits = stop_bits;
+    dcb.fAbortOnError = TRUE;
+    dcb.fBinary = TRUE;
+
+    /*  set XON/XOFF */
+    dcb.fOutX = FALSE;
+    dcb.fInX = FALSE;
+    dcb.fOutxCtsFlow = FALSE;
+    dcb.fRtsControl = RTS_CONTROL_DISABLE;
+    dcb.fOutxDsrFlow = FALSE;
+    dcb.fDtrControl = DTR_CONTROL_DISABLE;
+
+    bPortReady = SetCommState(hComm, &dcb);
+
+    /* Communication timeouts are optional */
+    CommTimeouts.ReadIntervalTimeout = 100;
+    CommTimeouts.ReadTotalTimeoutConstant = 0;
+    CommTimeouts.ReadTotalTimeoutMultiplier = 500;
+    CommTimeouts.WriteTotalTimeoutConstant = 0;
+    CommTimeouts.WriteTotalTimeoutMultiplier = 500;
+
+    if(!SetCommTimeouts (hComm, &CommTimeouts))
+    {
+        MSG(sd->log, "[ERROR] setup_port(SetCommTimeouts), failed! \n");
+        return false;
+    }
+        
+    if(!GetCommState(hComm, &dcb))
+    {
+        MSG(sd->log, "[ERROR] setup_port(GetCommState), failed! \n");
+        return false;
+    }
+
+    MSG(sd->log, "[INFO] setup_port, passed. \n");
+    return true;
 }
-#endif
+
+/**
+    Reading:
+    To tell windows that data is going to be send over the serial port, the handle, the data and the amount of data are needed.
+    The following transmission is a simple fileIO operation.
+ */
+
+DWORD uart_rx(HANDLE hComm, uint8_t * buffer, int buffersize)
+{
+    DWORD dwBytesRead = 0;
+    if(!ReadFile(hComm, buffer, buffersize, &dwBytesRead, NULL)){
+        //handle error
+    }
+    return dwBytesRead;
+}
+
+/**
+    Writing
+    The same information is needed when writing to the port.
+ */
+
+DWORD uart_tx(HANDLE hComm, uint8_t * data, int length)
+{
+
+    DWORD dwBytesRead = 0;
+    if(!WriteFile(hComm, data, length, &dwBytesRead, NULL)){
+        //handle error
+    }
+    return dwBytesRead;
+
+}
 
 bool motor_driver_init(system_data* sd)
 {
@@ -238,7 +189,7 @@ bool motor_driver_init(system_data* sd)
     if(sd->hComm ==INVALID_HANDLE_VALUE)
         return false;
         
-    rc = setup_port(sd->hComm, CBR_9600, 8, 0, false, false);
+    rc = setup_port(sd->hComm, CBR_9600, 8, NOPARITY, ONESTOPBIT);
     
     if(!rc)
         return false;
@@ -248,9 +199,24 @@ bool motor_driver_init(system_data* sd)
  
 bool motor_driver_update(system_data* sd)
 {
-    uint8_t  data = 0x5A;
+    uint8_t wb[128] = {STC1, STC2, 0xA3, 0x08,
+                        0x01, 0x00, 0x01, 0x00,
+                        0x64, 0x64, 0x64, 0x64};
+    
+    uint8_t rb[128] = {0}, rc = 0, i;
 
-    writeToSerialPort(sd->hComm, &data, 1);
+    wb[127] = bcc(wb, 12);
+    uart_tx(sd->hComm, wb, 12);
+    uart_tx(sd->hComm, &wb[127], 1);
+
+    rc = uart_rx(sd->hComm, rb,13);
+    
+    if(rc > 0)
+    {
+        for(i = 0 ; i < rc ; i++)
+            printf("0x%X \n", rb[i]);
+    }
+
     return true;
 }
 
