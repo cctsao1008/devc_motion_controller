faf8384dc57ba969d6e2fb2280ca99436cdbd1b3
cctsao1008 <chiacheng.tsao@gmail.com>
2016-07-14 01:29:47 +0800

    TAB to SPACE.
    Signed-off-by: cctsao1008 <chiacheng.tsao@gmail.com>

--- common/test/test.c
+++ common/test/test.c
@@ -12,25 +12,25 @@
 
 void test_kinematics(void)
 {
-	system_data* sd;
-	sd = system_init();
-	
-	TEST_CHECK(sd != NULL);
-	TEST_CHECK(kinematics_init(sd) != false);
-	
-	sd->cv.vx = 1;
-	sd->cv.vy = 1;
-	sd->cv.w0 = 1;
-    
+    system_data* sd;
+    sd = system_init();
+
+    TEST_CHECK(sd != NULL);
+    TEST_CHECK(kinematics_init(sd) != false);
+
+    sd->cv.vx = 1;
+    sd->cv.vy = 1;
+    sd->cv.w0 = 1;
+
     TEST_CHECK(inverse_kinematics(sd) != false);
-    
+
     sd->mot.in.w1 = sd->mot.out.w1;
     sd->mot.in.w2 = sd->mot.out.w2;
     sd->mot.in.w3 = sd->mot.out.w3;
     sd->mot.in.w4 = sd->mot.out.w4;
-    
+
     TEST_CHECK(forward_kinematics(sd) != false);
-    
+
     TEST_CHECK(sd->pv.vx != sd->sv.vx);
     TEST_CHECK(sd->pv.vy != sd->sv.vy);
     TEST_CHECK(sd->pv.w0 != sd->sv.w0);
